43626807d7ee6585cc761d237fbbecc34be08653
abdelkhalek hanbel <116838826+bhwayjoo@users.noreply.github.com>
2024-01-11 18:04:59 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,23 +1,5 @@
-
-# main_program.py
 import subprocess
-import threading
-import time
 
-def run_script(script_name):
-    subprocess.run(["python", script_name])
-
-def run_flask_app():
-    subprocess.run(["python", "telegram_bot.py"])
-
-if __name__ == "__main__":
-    # Start script1.py in a separate thread
-    thread_script1 = threading.Thread(target=run_script, args=("app4.py",))
-    thread_script1.start()
-
-    # Start script2.py in a separate thread
-    thread_script2 = threading.Thread(target=run_script, args=("bot4.py",))
-    thread_script2.start()
-
-    # Start Flask app in the main thread
-    run_flask_app()
+# Replace 'program1.py' and 'program2.py' with the names of your Python programs
+process1 = subprocess.Popen(['python', './bot4.py'])
+process2 = subprocess.Popen(['python', './app4.py'])
